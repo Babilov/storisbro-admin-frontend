e2b1ccbabc801e22f9faa4eb172dc0a510e03cf9
Babilov <egor-kun2017@mail.ru>
2025-09-13 12:37:53 +0300

    1

--- src/pages/Statistic/StatisticDetails.jsx
+++ src/pages/Statistic/StatisticDetails.jsx
@@ -1,20 +1,28 @@
 import { useLocation } from "react-router-dom";
 import MyContainer from "../../components/CommonComponents/MyContainer";
 import TitleDiv from "../../components/CommonComponents/TitleDiv";
+import { useEffect } from "react";
+import axios from "axios";
+import { API_URL } from "../../utils/constants";
 
 const StatisticDetails = () => {
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
-    console.log(queryParams)
     const dateFrom = queryParams.get("date_from");
     const dateTo = queryParams.get("date_to");
-    console.log(dateFrom, dateTo)
-    const title = `Статистика ${dateFrom} - ${dateTo}`
+    
+    useEffect(() => {
+        const getStatistic = async () => {
+            const res = await axios.get(`${API_URL}statistic/?date_from=${dateFrom}&date_to=${dateTo}`);
+            console.log(res)
+        }
+        getStatistic();
+    }, [dateFrom, dateTo])
 
     return (
         <MyContainer>
             <TitleDiv>
-                {title}
+                Статистика
             </TitleDiv>
         </MyContainer>
     )
