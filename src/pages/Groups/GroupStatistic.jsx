078146fdb35b2197396c28cf102daed34ad2d534
Babilov <egor-kun2017@mail.ru>
2025-09-15 12:27:46 +0300

    1

--- src/pages/Groups/GroupStatistic.jsx
+++ src/pages/Groups/GroupStatistic.jsx
@@ -4,6 +4,9 @@ import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 import MyButton from "../../components/CommonComponents/MyButton";
 import MyContainer from "../../components/CommonComponents/MyContainer";
+import axios from "axios";
+import { API_URL } from "../../utils/constants";
+import { useEffect, useState } from "react";
 
 const Div = styled.div`
   padding: 10px 50px;
@@ -33,11 +36,36 @@ const DateDiv = styled.div`
   margin: 20px 0;
 `;
 
+
+
+
+
+/*const handleClick = async () => {
+  const res = axios.get(`${API_URL}community/stats/${id}/date_from=${dateFrom}&date_to=${dateTo}`)
+
+}*/
+
+
+
 const GroupStatistic = () => {
   const location = useLocation();
   const { id } = useParams()
   console.log(id)
   const { title } = location.state || {};
+
+  const handleDateChange = (e, setDate) => {
+      setDate(e.target.value);
+  };
+
+  useEffect(() => {
+    console.log(`DATEfrom: ${dateFrom}`)
+    console.log(`DATEto: ${dateTo}`)
+  }, [dateFrom, dateTo])
+
+
+  const [dateFrom, setDateFrom] = useState('');
+  const [dateTo, setDateTo] = useState('');
+
   return (
     <MyContainer>
       <TitleDiv title="Статистика сообщества" />
@@ -53,7 +81,8 @@ const GroupStatistic = () => {
         </Div>
       </GroupInfoBlock>
       <DateDiv>
-        Сроки с <Input type="date" /> по <Input type="date" />
+        Сроки с <Input type="date" value={dateFrom} onChange={(e) => handleDateChange(e, setDateFrom)} /> 
+        по <Input type="date" value={dateTo} onChange={(e) => handleDateChange(e, setDateTo)} />
       </DateDiv>
       <MyButton sizes={{ width: "180px", height: "40px" }}>Показать</MyButton>
     </MyContainer>
