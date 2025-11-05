254b494335838d1d817f3e73493a178e5eb9bebe
Babilov <egor-kun2017@mail.ru>
2025-11-05 12:22:28 +0300

    1

--- src/App.js
+++ src/App.js
@@ -23,29 +23,28 @@ function App() {
         <Route path="/" element={<Login />} />
 
         {/* Главное меню и всё остальное под ним */}
-        <Route path="/menu">
-          <Route index element={<MainMenu />} />
-          <Route path="creatives" element={<Creatives />} />
-          <Route path="pricing" element={<Pricing />} />
-          <Route path="adlinks" element={<AdLinks />} />
-          <Route path="funds" element={<FundsRequests />} />
-          <Route path="statistic" element={<Statistic />} />
-          <Route path="statistics-details" element={<StatisticDetails />} />
+        <Route path="/menu" element={<MainMenu />} />
 
-          <Route path="groups">
-            <Route index element={<Groups />} />
-            <Route path="statistic/:id" element={<GroupStatistic />} />
-          </Route>
+        <Route path="creatives" element={<Creatives />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="adlinks" element={<AdLinks />} />
+        <Route path="funds" element={<FundsRequests />} />
+        <Route path="statistic" element={<Statistic />} />
+        <Route path="statistics-details" element={<StatisticDetails />} />
 
-          <Route path="users">
-            <Route index element={<UsersBase />} />
-            <Route path=":id" element={<UserStatistic />} />
-          </Route>
+        <Route path="groups">
+          <Route index element={<Groups />} />
+          <Route path="statistic/:id" element={<GroupStatistic />} />
+        </Route>
 
-          <Route path="publication_settings">
-            <Route index element={<PublicationSettings />} />
-            <Route path=":number" element={<AdPlace />} />
-          </Route>
+        <Route path="users">
+          <Route index element={<UsersBase />} />
+          <Route path=":id" element={<UserStatistic />} />
+        </Route>
+
+        <Route path="publication_settings">
+          <Route index element={<PublicationSettings />} />
+          <Route path=":number" element={<AdPlace />} />
         </Route>
       </Routes>
     </div>
